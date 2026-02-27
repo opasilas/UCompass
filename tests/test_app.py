5f21766116492a994a5c8f8f8e389067e07caf8d
hasanalrashed <hasanal-rashed@hotmail.com>
2026-02-27 09:47:17 +0000

    add unit test for student dashboard access

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,10 +13,28 @@ class RoutesTestCase(unittest.TestCase):
         # Test home page (login) loads correctly
         response = self.client.get('/')
 
-        # 200 means page loaded unsuccessfully
+        # 200 means page loaded successfully
         self.assertEqual(response.status_code, 200)
 
         # Check for 'Login'
+        self.assertIn(b'Login', response.data)
+
+    def test_student_dashboard_access(self):
+        with self.client.session_transaction() as sess:
+            # manually set session data that app expects
+            sess['user_email'] = 'student@example.com'
+            sess['user_role'] = 'student'
+
+        response = self.client.get('/student_dashboard')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'student_dashboard', response.data)
+
+    def test_student_dashboard_unauthorized(self):
+        # Test access is denied if not logged in
+        response = self.client.get('/student_dashboard', follow_redirects=True)
+
+        # It should redirect back to login
+        self.assertIn(b'Unauthorized access.', response.data)
         self.assertIn(b'Login', response.data)
 
 
